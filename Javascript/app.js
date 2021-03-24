6fbc1b114a750f1c906ccf3667d6279b7e3842cc
Anubhab Mukherjee <anubhabmukherjee5@gmail.com>
2021-03-24 20:28:42 +0530

    Js

---
+++ Javascript/app.js
@@ -0,0 +1,70 @@
+// // statement
+// // identifier
+// // syntax
+// // data types
+// // operators
+
+// // variable
+// // var firstName = "Jack";
+// // var lastName = "Ma";
+
+// // a-z, A-Z, 0-9, _ , $
+// // 0firstName 12334 _ a A
+
+// var $FirstNa34me111 = "Jack 1234ffff";
+// console.log($FirstNa34me111);
+
+// // console.log("hii");
+
+// // data types : primitive dtat
+// // 1. string
+// // 2. number
+// // 3. boolean
+// // 4. undefined
+// // 5. null
+
+// var firstName = "Jane"; // "";
+// console.log(firstName);
+// console.log(typeof firstName);
+
+// var lastName;
+
+// firstName = 21;
+// console.log(firstName);
+// console.log(typeof firstName);
+
+// var rollNumber = 11;
+// console.log(rollNumber);
+
+// var isPassed = true;
+// console.log(isPassed);
+
+// var isCovidPositive = false;
+// console.log(isCovidPositive);
+
+// lastName = "K";
+// console.log("Last Name is: " , lastName);
+
+// 1. Object
+
+// var firstName = "Jane";
+// firstName = "";
+// // console.log("firstName: " + firstName);
+
+// // console.log(typeof firstName);
+
+// var lastName = "Ds";
+// // console.log("Last Name: " + lastName);
+
+// var rollNumber;
+// console.log("rollNumber: " + rollNumber);
+// console.log("typeof rollNumber: " + typeof rollNumber);
+
+// rollNumber = 11;
+
+// console.log("rollNumber: " + rollNumber);
+
+// rollNumber = null;
+// console.log("typeof rollNumber: " + typeof rollNumber); // defect 
+
+
